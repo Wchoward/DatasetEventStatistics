c9c1b8f59e60b362a8ea2eb411f44f2775cbdc0c
Howard WU <Wchoward@users.noreply.github.com>
2020-05-05 01:09:49 +0800

    generate csv

--- process.py
+++ process.py
@@ -2,18 +2,22 @@
 import fm
 import os
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 
 def process_file(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
+    lst = []
     for sample in root:
         count = 0
         for event in sample[1].findall('event'):
             if (event.get('cause') == 'Y'):
                 count = count + 1
-        print(sample.get('id'), count)
+        lst.append([int(sample.get('id')), count, 0])
+    df = pd.DataFrame(lst, columns=['sample_id', 'verb_event_num', 'none_event_num'])
+    df.to_csv("data/result.csv", index=False)
 
 
 if __name__ == '__main__':
-    process_file('data/test.xml')
+    process_file('data/ImplicitECD.Tuple.forTag.xml')
