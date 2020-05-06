f9dfd84f60f205af16740cf4230d4118f8a411f7
Howard WU <Wchoward@users.noreply.github.com>
2020-05-06 18:40:18 +0800

    divide verb/noun cause

--- process.py
+++ process.py
@@ -24,21 +24,27 @@ def process_file(file_path):
     tree.write("data/data_without_Ncause.xml", encoding="utf-8", xml_declaration=True)
     df = pd.DataFrame(lst, columns=['sample_id', 'verb_event_num', 'noun_event_num'])
     # 生成原因整体数量统计表（全部当作动词事件）
-    df.to_csv("data/raw_cause_number_result.csv", index=False)
+    df.to_csv("data/all_cause_result.csv", index=False)
 
 
 def process_noun_event_table(dataset_path):
     noun_table = pd.read_csv('data/noun_table.csv')
-    result_table = pd.read_csv('data/raw_cause_number_result.csv')
+    result_table = pd.read_csv('data/all_cause_result.csv')
     tree = ET.parse(dataset_path)
     root = tree.getroot()
     for noun_row in noun_table.itertuples():
         sample_id = getattr(noun_row, 'sample_id')
         event_id = getattr(noun_row, 'event_id')
-        if root[sample_id - 1][1][event_id - 1].get('cause') == 'Y':
-            result_table.loc[sample_id - 1, 'noun_event_num'] = result_table.loc[sample_id - 1, 'noun_event_num'] + 1
-    # 生成抽取出名词事件的数量统计表,后续需要减一下
-    result_table.to_csv('data/cause_number_result.csv', index=False)
+        try:
+            if root[sample_id - 1][1][event_id - 1].get('cause') == 'Y':
+                result_table.loc[sample_id - 1, 'noun_event_num'] = result_table.loc[
+                                                                        sample_id - 1, 'noun_event_num'] + 1
+                result_table.loc[sample_id - 1, 'verb_event_num'] = result_table.loc[
+                                                                        sample_id - 1, 'verb_event_num'] - 1
+        except Exception:
+            print(sample_id, event_id)
+    # 生成抽取出动词/名词事件的数量统计表
+    result_table.to_csv('data/divide_cause_number_result.csv', index=False)
 
 
 if __name__ == '__main__':
