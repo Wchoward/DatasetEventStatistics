eedcf381c7e65df802f7db21bf4a2c9305d8bf8c
Howard WU <Wchoward@users.noreply.github.com>
2020-06-10 12:31:41 +0800

    add emotion label input in train

--- train.py
+++ train.py
@@ -5,14 +5,11 @@ import numpy as np
 import os
 import time
 import fm
-# import word2vector
 from sklearn.metrics import precision_recall_fscore_support
-
-# import tensorflow.contrib.eager as tfe
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "7"
 
-EPOCH = 8
+EPOCH = 7
 BATCH_SIZE = 16
 
 
@@ -24,24 +21,30 @@ def train_per_emotion(path, module1, module2):
     # SAMPLE_NUM = len(original_text)
     # original_text = tf.convert_to_tensor(original_text, dtype=tf.float32)
     original_text = original_text.astype(np.float32)
+
     cause_events = np.load(os.path.join(path, 'cause_event.npy'), allow_pickle=True)
     # cause_events = tf.convert_to_tensor(cause_events, dtype=tf.float32)
     cause_events = cause_events.astype(np.float32)
+
     labels = np.load(os.path.join(path, 'if_cause.npy'), allow_pickle=True)
     # labels = tf.convert_to_tensor(labels, dtype=tf.int32)
     # labels = np.one_hot(labels, depth=2)
     labels = labels.astype(np.int32)
     labels = np.eye(2)[labels]
 
+    emotion_labels = np.load(os.path.join(path, 'emotion_label.npy'), allow_pickle=True)
+    emotion_labels = emotion_labels.astype(np.float32)
+
     with tf.name_scope("input_module"):
         original_text_input = layers.Input(batch_shape=(None, 169, 300))
         event_input = layers.Input(batch_shape=(None, 44, 300))
+        emotion_labels_input = layers.Input(batch_shape=(None, 7))
         # predictions = layers.Input(batch_shape=(None, 1))
 
     net = MainModel()
-    output = net.model_build(module1, module2, original_text_input, event_input)
+    output = net.model_build(module1, module2, original_text_input, event_input, emotion_labels_input)
 
-    model = tf.keras.Model(inputs=[original_text_input, event_input], outputs=output)
+    model = tf.keras.Model(inputs=[original_text_input, event_input, emotion_labels_input], outputs=output)
     # model = tf.keras.Model()
 
     optimizer = tf.keras.optimizers.RMSprop(0.001)
@@ -62,19 +65,22 @@ def train_per_emotion(path, module1, module2):
     train_original_text = original_text[400:]
     train_cause_events = cause_events[400:]
     train_labels = labels[400:]
+    train_emotion_labels = emotion_labels[400:]
     test_original_text = original_text[:400]
     test_cause_events = cause_events[:400]
     test_labels = labels[:400]
+    test_emotion_labels = emotion_labels[:400]
 
-    model.fit(x=[train_original_text, train_cause_events], y=train_labels, epochs=EPOCH, shuffle=True,
-              validation_data=([test_original_text, test_cause_events], test_labels), batch_size=16)
+    model.fit(x=[train_original_text, train_cause_events, train_emotion_labels], y=train_labels, epochs=EPOCH,
+              shuffle=True, validation_data=([test_original_text, test_cause_events, test_emotion_labels], test_labels),
+              batch_size=16)
 
     cur_time = time.localtime(time.time())
     # model.save(os.path.join('model/my_model',
     #                         'my_model_{}_{}_{}_{}_{}.h5'.format(cur_time.tm_mon, cur_time.tm_mday, cur_time.tm_hour,
     #                                                             cur_time.tm_min, cur_time.tm_sec)))
 
-    prediction = model.predict([test_original_text, test_cause_events])
+    prediction = model.predict([test_original_text, test_cause_events, test_emotion_labels])
     true_labels = np.argmax(test_labels, axis=1)
     pred_labels = np.argmax(prediction, axis=1)
 
@@ -92,7 +98,7 @@ def get_dir_list(dir_path):
     return lst
 
 
-def train_lst(root_path, category_lst, module):
+def train_lst_category(root_path, category_lst, module):
     res = []
     for module1 in module:
         for module2 in module:
@@ -107,31 +113,52 @@ def train_lst(root_path, category_lst, module):
     return res
 
 
+def train_lst_merge(root_path, module):
+    res = []
+    for module1 in module:
+        for module2 in module:
+            res.append(module1 + '+' + module2 + ',,,')
+            res.append('lambda,P,R,F')
+            for i in range(0, 101, 25):
+                path = root_path + str(i / 100)
+                lambda_p_r_f = [str(i / 100)] + train_per_emotion(path, module1, module2)
+                s_lambda_p_r_f = ','.join(str(i) for i in lambda_p_r_f)
+                res.append(s_lambda_p_r_f)
+            res.append('')
+    return res
+
+
 if __name__ == '__main__':
     root_path = 'data/origin_w2v_emotion_category'
-    module = ['cnn', 'lstm', 'attention']
+    module = ['attention']
     category_lst = get_dir_list(root_path)
-    # train_per_emotion('data/merge_expand_emotion_embedding_1', 'attention', 'attention')
+    # train_per_emotion('data/merge_emotion_embedding_0', 'attention', 'attention')
+
+    # root_path = 'data/merge_expand_emotion_embedding_'
+    # result = train_lst_merge(root_path, module)
+    # fm.save_file('result/merge_expand_emotion_label.txt', result)
+
     # origin w2v
-    # result = train_lst(root_path, category_lst, module)
-    # fm.save_file('result/origin_result.txt', result)
-    # lambda=0
-    root_path = 'data/expand_w2v_emotion_category_lambda_0'
-    result = train_lst(root_path, category_lst, module)
-    fm.save_file('result/expand_lambda_result_0.txt', result)
-    # lambda=0.25
-    root_path = 'data/expand_w2v_emotion_category_lambda_0.25'
-    result = train_lst(root_path, category_lst, module)
-    fm.save_file('result/expand_lambda_result_0.25.txt', result)
-    # lambda=0.5
-    root_path = 'data/expand_w2v_emotion_category_lambda_0.5'
-    result = train_lst(root_path, category_lst, module)
-    fm.save_file('result/expand_lambda_result_0.5.txt', result)
-    # lambda=0.75
-    root_path = 'data/expand_w2v_emotion_category_lambda_0.75'
-    result = train_lst(root_path, category_lst, module)
-    fm.save_file('result/expand_lambda_result_0.75.txt', result)
-    # lambda=1
-    root_path = 'data/expand_w2v_emotion_category_lambda_1'
-    result = train_lst(root_path, category_lst, module)
-    fm.save_file('result/expand_lambda_result_1.txt', result)
+    result = train_lst_category(root_path, category_lst, module)
+    fm.save_file('result/origin_result_emotion_label.txt', result)
+
+    # # lambda=0
+    # root_path = 'data/expand_w2v_emotion_category_lambda_0'
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/expand_lambda_result_0.txt', result)
+    # # lambda=0.25
+    # root_path = 'data/expand_w2v_emotion_category_lambda_0.25'
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/expand_lambda_result_0.25.txt', result)
+    # # lambda=0.5
+    # root_path = 'data/expand_w2v_emotion_category_lambda_0.5'
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/expand_lambda_result_0.5.txt', result)
+    # # lambda=0.75
+    # root_path = 'data/expand_w2v_emotion_category_lambda_0.75'
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/expand_lambda_result_0.75.txt', result)
+    # # lambda=1
+    # root_path = 'data/expand_w2v_emotion_category_lambda_1'
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/expand_lambda_result_1.txt', result)
