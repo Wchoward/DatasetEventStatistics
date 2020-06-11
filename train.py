6517ee47246f92c34755ded7d128503829a07ae4
Howard WU <Wchoward@users.noreply.github.com>
2020-06-11 20:31:49 +0800

    Adjustable parameter

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from sklearn.metrics import precision_recall_fscore_support
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "7"
 
-EPOCH = 7
+EPOCH = 9
 BATCH_SIZE = 16
 
 
@@ -47,7 +47,7 @@ def train_per_emotion(path, module1, module2):
     model = tf.keras.Model(inputs=[original_text_input, event_input, emotion_labels_input], outputs=output)
     # model = tf.keras.Model()
 
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    optimizer = tf.keras.optimizers.RMSprop(0.0001)
     model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
     model.summary()
@@ -132,33 +132,33 @@ if __name__ == '__main__':
     root_path = 'data/origin_w2v_emotion_category'
     module = ['attention']
     category_lst = get_dir_list(root_path)
-    # train_per_emotion('data/merge_emotion_embedding_0', 'attention', 'attention')
+    train_per_emotion('data/merge_origin_emotion_embedding', 'attention', 'attention')
 
-    # root_path = 'data/merge_expand_emotion_embedding_'
+    # root_path = 'data/merge_emotion_embedding_'
     # result = train_lst_merge(root_path, module)
-    # fm.save_file('result/merge_expand_emotion_label.txt', result)
+    # fm.save_file('result/merge_emotion_label.txt', result)
 
-    # origin w2v
-    result = train_lst_category(root_path, category_lst, module)
-    fm.save_file('result/origin_result_emotion_label.txt', result)
+    # # origin w2v
+    # result = train_lst_category(root_path, category_lst, module)
+    # fm.save_file('result/origin_result_emotion_label.txt', result)
 
     # # lambda=0
-    # root_path = 'data/expand_w2v_emotion_category_lambda_0'
+    # root_path = 'data/w2v_emotion_category_lambda_0'
     # result = train_lst_category(root_path, category_lst, module)
-    # fm.save_file('result/expand_lambda_result_0.txt', result)
+    # fm.save_file('result/lambda_result_0_emotion_label.txt', result)
     # # lambda=0.25
-    # root_path = 'data/expand_w2v_emotion_category_lambda_0.25'
+    # root_path = 'data/w2v_emotion_category_lambda_0.25'
     # result = train_lst_category(root_path, category_lst, module)
-    # fm.save_file('result/expand_lambda_result_0.25.txt', result)
+    # fm.save_file('result/lambda_result_0.25_emotion_label.txt', result)
     # # lambda=0.5
-    # root_path = 'data/expand_w2v_emotion_category_lambda_0.5'
+    # root_path = 'data/w2v_emotion_category_lambda_0.5'
     # result = train_lst_category(root_path, category_lst, module)
-    # fm.save_file('result/expand_lambda_result_0.5.txt', result)
+    # fm.save_file('result/lambda_result_0.5_emotion_label.txt', result)
     # # lambda=0.75
-    # root_path = 'data/expand_w2v_emotion_category_lambda_0.75'
+    # root_path = 'data/w2v_emotion_category_lambda_0.75'
     # result = train_lst_category(root_path, category_lst, module)
-    # fm.save_file('result/expand_lambda_result_0.75.txt', result)
+    # fm.save_file('result/lambda_result_0.75_emotion_label.txt', result)
     # # lambda=1
-    # root_path = 'data/expand_w2v_emotion_category_lambda_1'
+    # root_path = 'data/w2v_emotion_category_lambda_1'
     # result = train_lst_category(root_path, category_lst, module)
-    # fm.save_file('result/expand_lambda_result_1.txt', result)
+    # fm.save_file('result/lambda_result_1_emotion_label.txt', result)
